ee470819506804fdb2624a37ff3e91ccae6a354a
yamacho <yamacho@users.noreply.github.com>
2017-11-21 21:54:58 +0900

    画面のマークアップをしていこう
    * html のマークアップ
    * 要素の取得

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  var reset = document.getElementById('reset');
+})();
